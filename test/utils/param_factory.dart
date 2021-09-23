d460c4f67fc021da4144f6a490be142213930d05
codigobase <manuel.perez@codigobase.es>
2021-09-23 12:49:02 +0200

    Added test for TimeList widget

--- test/utils/param_factory.dart
+++ test/utils/param_factory.dart
@@ -4,6 +4,13 @@ abstract class ParamFactory {
   // Strings
   static const String time = '10:50';
 
+  // int
+  static const int timeStep = 10;
+
+  // TimeOfDay
+  static const TimeOfDay firstTime = TimeOfDay(hour: 10, minute: 10);
+  static const TimeOfDay secondTime = TimeOfDay(hour: 10, minute: 40);
+
   // Colors
   static const Color blue = Colors.blue;
   static const Color purple = Colors.purple;
